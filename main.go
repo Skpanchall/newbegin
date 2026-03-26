563328dac047739f6358bce5bdc340d8aa911b96
Sachin panchal <panchalsachin989845@gmail.com>
2026-03-26 23:48:47 +0530

    variable scan and show bio for person

--- main.go
+++ main.go
@@ -3,5 +3,24 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("hello world new one ")
+	var name string
+	var age int
+	var city string
+	var isWorking bool
+	var salary int
+	fmt.Println("what is your name ?")
+	fmt.Scanln(&name)
+	fmt.Println("age ? ")
+	fmt.Scanln(&age)
+	fmt.Println("city ? ")
+	fmt.Scanln(&city)
+	fmt.Println("working ? ")
+	fmt.Scanln(&isWorking)
+	fmt.Println("salary ? ")
+	fmt.Scanln(&salary)
+	fmt.Printf("so now your bio is : \nname : %s \nage:%d  \ncity:%s  \n")
 }
+
+// day 1
+// learn a variable how to declar
+//
